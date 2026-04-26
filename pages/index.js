094b734d1d24acbcb7f28d8f49328140b58d8b88
paisasochs-art <paisasochs@gmail.com>
2026-04-26 15:42:32 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>PaisaSoch Routing Layer</h1>
+      <p>Middleware is active.</p>
+    </div>
+  );
+}
+
+export default Home;
