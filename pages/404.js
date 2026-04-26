dc0cc1729eb9536af290b70f37e7a799e1bdb1ae
paisasochs-art <paisasochs@gmail.com>
2026-04-26 15:42:00 +0530

    Create 404.js

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Custom404() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>404 - Not Found</h1>
+      <p>This route is handled by middleware.</p>
+    </div>
+  );
+}
+
+export default Custom404;
